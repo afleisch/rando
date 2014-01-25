f20dae41e0f010b6d9c4a7dbe20ecc1a2c9caa5a
afleisch <afleisch@gmail.com>
2014-01-25 13:02:09 -0800

    first commit

---
+++ test/controllers/randos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RandosControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/helpers/randos_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class RandosHelperTest < ActionView::TestCase
+end

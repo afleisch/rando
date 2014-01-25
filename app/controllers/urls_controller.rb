5f6e547816e1da4a61605dc44f818c276d0fd77f
afleisch <afleisch@gmail.com>
2014-01-25 15:24:21 -0800

    updated #create to add random string into db

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -8,4 +8,15 @@ class UrlsController < ApplicationController
  def new
   @url = Url.new
  end
+
+
+ def create
+ 	@random_string = SecureRandom.urlsafe_base64(10)
+	new_url= params.require(:url).permit(:link)
+	url = Url.create(new_url) do |u|
+		u.random_string = @random_string
+	redirect_to "/urls"
+end
+end
+
 end

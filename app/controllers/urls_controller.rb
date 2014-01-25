c67b20d4b5fce6f0d51b4737ce78fc219256b50a
afleisch <afleisch@gmail.com>
2014-01-25 14:47:06 -0800

    updated #new

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,11 @@
 class UrlsController < ApplicationController
 
  def index 
- @urls = Url.all
-  render :index
-
+   @urls = Url.all
+   render :index
  end
 
-
+ def new
+  @url = Url.new
+ end
 end

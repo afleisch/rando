5c4836f049409272baf9694665e5174eaba3cabe
afleisch <afleisch@gmail.com>
2014-01-25 14:15:03 -0800

    first commit

--- db/migrate/20140125214540_create_urls.rb
+++ db/migrate/20140125214540_create_urls.rb
@@ -1,6 +1,8 @@
 class CreateUrls < ActiveRecord::Migration
   def change
     create_table :urls do |t|
+      t.string :link
+      t.string :random_string
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140125211824) do
+ActiveRecord::Schema.define(version: 20140125214540) do
 
   create_table "urls", force: true do |t|
+    t.string   "link"
+    t.string   "random_string"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

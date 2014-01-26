d2f01fc9f889feea759b3e22d950e8b8fe684ac1
afleisch <afleisch@gmail.com>
2014-01-25 16:40:43 -0800

    deleted edit - not needed

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -25,10 +25,10 @@ class UrlsController < ApplicationController
 	render :show
  end
 
- def edit
-	id = params[:id]
-	@url = Url.find(id)
-	render :edit	
- end
+ # def edit
+	# id = params[:id]
+	# @url = Url.find(id)
+	# render :edit	
+ # end
 
 end
